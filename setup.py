03071bfdea4cba5c107974228ad701bc5c777ea6
javapagar <apariciogarcia.j@gmail.com>
2021-05-14 07:42:51 +0200

    setup requirements

--- setup.py
+++ setup.py
@@ -18,8 +18,11 @@ setup(
           'plotly',
           'seaborn',
           'scipy',
-          'sklearn'
-
+          'sklearn',
+          'folium',
+          'requests',
+          'wordcloud',
+          'phik'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
